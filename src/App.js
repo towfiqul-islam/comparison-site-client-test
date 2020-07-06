13880f3c917e5887f8d1cd37cee49b06a1834d0f
towfiqu <towfiqu@gmail.com>
2020-07-06 17:45:07 +0600

    final check

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Hello <code>src/App.js</code> React.
+          Automation is awesome!! <code>src/App.js</code> React.
         </p>
         <a
           className='App-link'

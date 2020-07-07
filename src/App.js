ca4b7c6f4e4f35cc71d85e346c7bfac1c47a5f9c
towfiqu <towfiqu@gmail.com>
2020-07-07 11:41:52 +0600

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1>Hello React</h1>
-      </header>
+    <div>
+      <h1>Hello React</h1>
     </div>
   );
 }
